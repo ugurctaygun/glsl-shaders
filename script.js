ddd7beab667605c0528110ecbcc55bd1474a5ed2
ugurctaygun <ugurctaygun@gmail.com>
2022-08-12 13:02:22 +0300

    first shader

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const vshader = `
+void main(){
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 0.5 , 1.0);
+}
+`;
+
+const fshader = `
+void main(){
+  gl_FragColor = vec4(1.0,1.0,0.0,1.0);
+}
+`;
+
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.PlaneGeometry(2, 2);
+const material = new THREE.ShaderMaterial({
+  vertexShader: vshader,
+  fragmentShader: fshader,
+});
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
+camera.position.z = 1;
+onWindowResize();
+animate();
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);

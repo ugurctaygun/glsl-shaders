0e4397e89a6766579d1f472b6bb5d47388725d8c
ugurctaygun <ugurctaygun@gmail.com>
2023-01-01 23:15:17 +0300

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Scene from "./Scene";
+
+function App() {
+  return (
+    <div className="App">
+      <Scene />
+    </div>
+  );
+}
+
+export default App;

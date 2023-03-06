a307b041c79e45b3be60ec99b0d08ef5fdcfe014
ugurctaygun <ugurctaygun@gmail.com>
2023-03-06 22:49:55 +0300

    color blend

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Scene from "./Scene";
+import BlendingColors from "./Shaders/BlendingColors";
 
 function App() {
   return (
     <div className="App">
       <Scene />
+      <BlendingColors />
     </div>
   );
 }
